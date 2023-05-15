3a12e6478a959e640af65f3243f6ded6cc50828d
Marcone-Santos1 <marconemagalhaes203@gmail.com>
2023-05-15 12:07:25 -0300

    teste

--- nomal_captcha/version.py
+++ nomal_captcha/version.py
@@ -4,12 +4,12 @@ import toml
 dynamic_version = "1.0.0"
 
 # Load the original pyproject.toml
-with open("pyproject.toml", "r") as f:
+with open("../pyproject.toml", "r") as f:
     data = toml.load(f)
 
 # Update the version field with the dynamic version
 data["project"]["version"] = dynamic_version
 
 # Write the updated pyproject.toml
-with open("pyproject.toml", "w") as f:
+with open("../pyproject.toml", "w") as f:
     toml.dump(data, f)
